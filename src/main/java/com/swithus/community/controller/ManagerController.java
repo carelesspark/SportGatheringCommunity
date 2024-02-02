7dbcef3fc2c26af71d67a6c6b41b46db2121c72c
carelesspark <kiae1234@naver.com>
2024-02-02 17:05:43 +0900

    # 프로젝트 생성

---
+++ src/main/java/com/swithus/community/controller/ManagerController.java
@@ -0,0 +1,17 @@
+package com.swithus.community.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ManagerController {
+
+    @GetMapping("/")
+    public String index(){
+
+        return "manager/managerMain";
+    }
+}
