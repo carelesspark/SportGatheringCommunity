c56a9447ebf2b36ffa6198d4bfbe54fff5f0ade3
xodbs97 <xodbs97@gmail.com>
2024-02-06 18:10:52 +0900

    global 기본 폴더, 파일 생성

---
+++ src/main/java/com/swithus/community/global/config/WebMvcConfig.java
@@ -0,0 +1,14 @@
+package com.swithus.community.global.config;
+
+import com.swithus.community.global.interceptor.UserInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+//        registry.addInterceptor(new UserInterceptor()).addPathPatterns("/user/**");
+    }
+}
