0831c342910db0466019aad91b3e700dfbce6771
xodbs97 <xodbs97@gmail.com>
2024-02-08 18:27:53 +0900

    club 기본 화면 구성 추가
    club 기본 화면 구성 추가

--- src/main/java/com/swithus/community/club/controller/ClubController.java
+++ src/main/java/com/swithus/community/club/controller/ClubController.java
@@ -3,11 +3,22 @@ package com.swithus.community.club.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @Log4j2
 @RequiredArgsConstructor
-@RequestMapping("/static/css/club")
+@RequestMapping("/club")
 public class ClubController {
+
+    @GetMapping("/search")
+    public void goSearch(){
+        log.info("GET /club/search");
+    }
+
+    @GetMapping("/main")
+    public void goMain() {
+        log.info("GET /club/main");
+    }
 }
