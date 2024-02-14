d2622a90e6d2041e716f4758b5a2d0fc36a31f53
carelesspark <kiae1234@naver.com>
2024-02-14 18:47:08 +0900

    # 관리자 페이지 헤더&푸터 설정

--- src/main/java/com/swithus/community/manager/entity/ReportComment.java
+++ src/main/java/com/swithus/community/manager/entity/ReportComment.java
@@ -1,29 +1,29 @@
-package com.swithus.community.manager.entity;
-
-import com.swithus.community.global.entity.BaseEntity;
-import jakarta.persistence.*;
-import lombok.*;
-
-@Entity
-@Builder
-@AllArgsConstructor
-@NoArgsConstructor
-@Getter
-@ToString
-public class ReportComment extends BaseEntity {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long key;
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    private User reporter_key;
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    private Promotion reply_key;
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    private ReportCommentCtgr ctgr_key;
-
-    private String reason;
-
-}
+//package com.swithus.community.manager.entity;
+//
+//import com.swithus.community.global.entity.BaseEntity;
+//import jakarta.persistence.*;
+//import lombok.*;
+//
+//@Entity
+//@Builder
+//@AllArgsConstructor
+//@NoArgsConstructor
+//@Getter
+//@ToString
+//public class ReportComment extends BaseEntity {
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private Long key;
+//
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    private User reporter_key;
+//
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    private Promotion reply_key;
+//
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    private ReportCommentCtgr ctgr_key;
+//
+//    private String reason;
+//
+//}
--- src/main/java/com/swithus/community/manager/entity/ReportPost.java
+++ src/main/java/com/swithus/community/manager/entity/ReportPost.java
@@ -1,30 +1,30 @@
-package com.swithus.community.manager.entity;
-
-import com.swithus.community.global.entity.BaseEntity;
-import jakarta.persistence.*;
-import lombok.*;
-
-@Entity
-@Builder
-@AllArgsConstructor
-@NoArgsConstructor
-@Getter
-@ToString
-public class ReportPost extends BaseEntity {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long key;
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    private User reporter_key;
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    private Promotion post_key;
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    private ReportPostCtgr ctgr_key;
-
-    private String reason;
-
-}
+//package com.swithus.community.manager.entity;
+//
+//import com.swithus.community.global.entity.BaseEntity;
+//import jakarta.persistence.*;
+//import lombok.*;
+//
+//@Entity
+//@Builder
+//@AllArgsConstructor
+//@NoArgsConstructor
+//@Getter
+//@ToString
+//public class ReportPost extends BaseEntity {
+//
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private Long key;
+//
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    private User reporter_key;
+//
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    private Promotion post_key;
+//
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    private ReportPostCtgr ctgr_key;
+//
+//    private String reason;
+//
+//}
