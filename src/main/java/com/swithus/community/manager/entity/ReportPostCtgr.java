4e3e6f535a649af8beda042f9893267fb6d80294
carelesspark <kiae1234@naver.com>
2024-02-14 16:42:49 +0900

    # manager 엔티티 작성

---
+++ src/main/java/com/swithus/community/manager/entity/ReportPostCtgr.java
@@ -0,0 +1,22 @@
+package com.swithus.community.manager.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@ToString
+public class ReportPostCtgr {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long key;
+
+    private String ctgr_type;
+}
