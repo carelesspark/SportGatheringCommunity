196b6aa1df336c1220d22ef5c9a155a6dafd7466
xodbs97 <xodbs97@gmail.com>
2024-02-14 18:16:36 +0900

    club entity 기초 수정
    club entity 기초 수정

--- src/main/java/com/swithus/community/club/entity/Club.java
+++ src/main/java/com/swithus/community/club/entity/Club.java
@@ -1,6 +1,28 @@
 package com.swithus.community.club.entity;
 
 import com.swithus.community.global.entity.BaseEntity;
+import com.swithus.community.global.entity.Region;
+import com.swithus.community.global.entity.Sports;
 
 public class Club extends BaseEntity {
+
+    private Long key;
+
+    // 클럽장 키
+
+    // 종목 키
+    private Sports sports;
+    // 지역 키
+    private Region region;
+
+    // 클럽명
+    private String name;
+    // 클럽 소개
+    private String introduce;
+    // 클럽 인원
+    private int personnel;
+    // 클럽 랭크
+    private int rank;
+    // 클럽 포인트
+    private int point;
 }
