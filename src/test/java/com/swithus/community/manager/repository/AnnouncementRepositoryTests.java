19da81d0d4d6518a4f925baa7188c23e06825463
carelesspark <kiae1234@naver.com>
2024-02-15 16:56:40 +0900

    # 관리자 공지사항 dto&repository&service 부분 수정 중

---
+++ src/test/java/com/swithus/community/manager/repository/AnnouncementRepositoryTests.java
@@ -0,0 +1,38 @@
+package com.swithus.community.manager.repository;
+
+import com.swithus.community.manager.entity.Announcement;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+import java.util.stream.IntStream;
+
+@SpringBootTest
+public class AnnouncementRepositoryTests {
+
+    @Autowired
+    AnnouncementRepository announcementRepository;
+
+    @Test
+    public void insertAnnouncement(){
+        IntStream.rangeClosed(1, 100).forEach(i -> {
+            Announcement announcement = Announcement.builder()
+                    .content("Test Content")
+                    .title("Test Title")
+                    .visit_count(0)
+                    .writer("Manager").build();
+
+            announcementRepository.save(announcement);
+        });
+    }
+
+
+    @Test
+    public void getAnnouncement(){
+        Announcement announcement = Announcement.builder().key(98L).build();
+
+
+    }
+
+}
