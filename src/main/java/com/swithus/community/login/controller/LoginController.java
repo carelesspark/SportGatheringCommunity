a3ea660755be0957762810c52ec5af810ca4af3d
JB9778 <najungbae56@naver.com>
2024-02-16 18:35:15 +0900

    로그인 기능 구현중

--- src/main/java/com/swithus/community/login/controller/LoginController.java
+++ src/main/java/com/swithus/community/login/controller/LoginController.java
@@ -4,17 +4,29 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 @Controller
 @RequestMapping("/login")
 @Log4j2
 @RequiredArgsConstructor
 public class LoginController {
-    @GetMapping("/main")
+    @GetMapping("/login")
     public String login(){
 
         return "login/login";
     }
+
+    @PostMapping("/main")
+    public String login(@RequestParam String email, @RequestParam String password) {
+        // 여기에 Spring Security를 사용한 로그인 처리 로직 추가
+        // 예: AuthenticationManager를 사용하여 로그인 시도
+
+        return "/main/main"; // 로그인 후 리다이렉트할 경로
+    }
+
 
     @GetMapping("/findId")
     public String findId(){
@@ -33,4 +45,7 @@ public class LoginController {
 
         return "login/changePwd";
     }
+
+
+
 }
