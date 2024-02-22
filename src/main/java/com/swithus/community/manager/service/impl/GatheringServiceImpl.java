8bdab7be4e3394dcfcafa2fdb925b61cb712038b
carelesspark <kiae1234@naver.com>
2024-02-22 19:10:58 +0900

    # 관리자 소모임&삭제된 소모임 관리 페이지 추가

--- src/main/java/com/swithus/community/manager/service/impl/GatheringServiceImpl.java
+++ src/main/java/com/swithus/community/manager/service/impl/GatheringServiceImpl.java
@@ -5,9 +5,13 @@ import com.querydsl.core.types.dsl.BooleanExpression;
 import com.swithus.community.club.entity.Club;
 import com.swithus.community.club.entity.QClub;
 import com.swithus.community.manager.dto.GatheringDTO;
+import com.swithus.community.manager.dto.WithdrawalGatheringDTO;
 import com.swithus.community.manager.dto.page.GatheringPageRequestDTO;
 import com.swithus.community.manager.dto.page.GatheringPageResultDTO;
+import com.swithus.community.manager.entity.QWithdrawalGathering;
+import com.swithus.community.manager.entity.WithdrawalGathering;
 import com.swithus.community.manager.repository.GatheringRepository;
+import com.swithus.community.manager.repository.WithdrawalGatheringRepository;
 import com.swithus.community.manager.service.GatheringService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
@@ -26,6 +30,7 @@ public class GatheringServiceImpl implements GatheringService {
 
 
     private final GatheringRepository gatheringRepository;
+    private final WithdrawalGatheringRepository withdrawalGatheringRepository;
 
 
     @Override
@@ -67,4 +72,54 @@ public class GatheringServiceImpl implements GatheringService {
 
         return result.isPresent()? entityToDto(result.get()) : null;
     }
+
+    @Override
+    public void withdrawalGathering(WithdrawalGatheringDTO withdrawalGatheringDTO) {
+        WithdrawalGathering entity = dtoToEntity(withdrawalGatheringDTO);
+        withdrawalGatheringRepository.save(entity);
+        gatheringRepository.deleteById(withdrawalGatheringDTO.getClubId());
+    }
+
+    @Override
+    public GatheringPageResultDTO<WithdrawalGatheringDTO, WithdrawalGathering> getDeletedGatheringList(GatheringPageRequestDTO gatheringPageRequestDTO) {
+        Pageable pageable = gatheringPageRequestDTO.getPageable(Sort.by("id").descending());
+
+        BooleanBuilder booleanBuilder = getSearchDeletedGathering(gatheringPageRequestDTO);
+
+        Page<WithdrawalGathering> result = withdrawalGatheringRepository.findAll(booleanBuilder, pageable);
+
+        Function<WithdrawalGathering, WithdrawalGatheringDTO> fn = (entity -> entityToWithdrawalGatheringDTO(entity));
+
+        return new GatheringPageResultDTO(result, fn);
+    }
+
+
+    private BooleanBuilder getSearchDeletedGathering(GatheringPageRequestDTO requestDTO){
+        String search = requestDTO.getSearch();
+        BooleanBuilder booleanBuilder = new BooleanBuilder();
+        QWithdrawalGathering qWithdrawalGathering = QWithdrawalGathering.withdrawalGathering;
+
+        BooleanExpression expression = qWithdrawalGathering.id.gt(0L);
+        booleanBuilder.and(expression);
+
+        if(search == null || search.trim().length() == 0){
+            return booleanBuilder;
+        }
+
+        BooleanBuilder conditionBuilder = new BooleanBuilder();
+        conditionBuilder.or(qWithdrawalGathering.name.contains(search));
+
+        booleanBuilder.and(conditionBuilder);
+
+        return booleanBuilder;
+    }
+
+    @Override
+    public WithdrawalGatheringDTO infoDeletedGathering(long no) {
+        Optional<WithdrawalGathering> result = withdrawalGatheringRepository.findById(no);
+
+        return result.isPresent()? entityToWithdrawalGatheringDTO(result.get()) : null;
+    }
 }
+
+
