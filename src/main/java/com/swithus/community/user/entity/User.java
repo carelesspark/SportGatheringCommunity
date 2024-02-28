e431ac3b3d03caec9bd1844e959db0f804c5bbc5
carelesspark <kiae1234@naver.com>
2024-02-28 21:37:28 +0900

    # 수정

--- src/main/java/com/swithus/community/user/entity/User.java
+++ src/main/java/com/swithus/community/user/entity/User.java
@@ -85,10 +85,4 @@ public class User extends BaseEntity {
         this.oAuthProvider = oAuthProvider;
     }
 
-    @Builder
-    public User(String email, String nickname, OAuthProvider oAuthProvider) {
-        this.email = email;
-        this.nickname = nickname;
-        this.oAuthProvider = oAuthProvider;
-    }
 }
